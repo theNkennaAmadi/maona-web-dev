5657aa1e8e20d1e2d05ac68d87a5fb8927d95fe6
theNkennaAmadi <nkenna@theblackpepperstudio.com>
2026-07-23 16:33:40 +0100

    feat: enhance landing page animations with adjusted durations and delays

--- landing.js
+++ landing.js
@@ -1,8 +1,12 @@
 const tl = gsap.timeline();
 tl.fromTo('.intro-heading', {clipPath: 'inset(0 100% 0 0)'}, {
     clipPath: 'inset(0 0% 0 0)',
-    duration: 2,
-    ease: 'power2.out'
+    duration: 3.5,
 });
-tl.fromTo('.intro-link', {clipPath: 'inset(0 100% 0 0)'}, {clipPath: 'inset(0 0% 0 0)', duration: 0.5})
-tl.fromTo('.lp', {clipPath: 'inset(0 100% 0 0)'}, {clipPath: 'inset(0 0% 0 0)', stagger: 0.25, duration: 1.2})
+tl.fromTo('.intro-link', {clipPath: 'inset(0 100% 0 0)'}, {clipPath: 'inset(0 0% 0 0)', duration: 0.85})
+tl.fromTo('.lp', {clipPath: 'inset(0 100% 0 0)'}, {
+    clipPath: 'inset(0 0% 0 0)',
+    stagger: 0.25,
+    duration: 1.25,
+    delay: 0.35
+})
